873010cb21e580a8d0c0f1c57fc95a2aca465870
Salim Balòc Kòhgradi <soltansalim97@gmail.com>
2025-07-27 21:18:51 +0330

    Add files via upload

---
+++ iur-image-url-replacement/admin/js/admin-settings.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function($) {
+    // نمایش/مخفی کردن فیلدهای API بر اساس سرویس انتخاب شده
+    function toggleApiFields() {
+        const method = $('#iur-upload-method').val();
+        $('.iur-service-field').hide();
+        
+        if (method === 'freeimage') {
+            $('[data-service="freeimage"]').show();
+        } 
+        else if (method === 'imgbb') {
+            $('[data-service="imgbb"]').show();
+        }
+        else if (method === 'cloudinary') {
+            $('[data-service="cloudinary"]').show();
+        }
+    }
+
+    // اجرای اولیه
+    toggleApiFields();
+    $('#iur-upload-method').change(toggleApiFields);
+
+    // مدیریت ریست تنظیمات
+    $('#iur-reset-settings').on('click', function(e) {
+        e.preventDefault();
+        
+        if (confirm(iurSettings.i18n.confirmReset)) {
+            $.post(ajaxurl, {
+                action: 'iur_reset_settings',
+                _wpnonce: iurSettings.nonce
+            }, function(response) {
+                if (response.success) {
+                    location.reload();
+                } else {
+                    alert('Error: ' + response.data);
+                }
+            });
+        }
+    });
+});
