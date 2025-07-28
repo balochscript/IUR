612b5b76aed8f13f9fc0204d83b0da9316853993
Salim Balòc Kòhgradi <soltansalim97@gmail.com>
2025-07-28 17:11:11 +0330

    Update admin-settings.js

--- iur-image-url-replacement/admin/js/admin-settings.js
+++ iur-image-url-replacement/admin/js/admin-settings.js
@@ -1,35 +1,37 @@
 jQuery(document).ready(function($) {
-    // Show/hide API fields based on selected service
+    // Show/hide API fields based on the selected service
     function toggleApiFields() {
         const method = $('#iur-upload-method').val();
         $('.iur-service-field').hide();
-
+        
         if (method === 'freeimage') {
             $('[data-service="freeimage"]').show();
-        } else if (method === 'imgbb') {
+        } 
+        else if (method === 'imgbb') {
             $('[data-service="imgbb"]').show();
-        } else if (method === 'cloudinary') {
+        }
+        else if (method === 'cloudinary') {
             $('[data-service="cloudinary"]').show();
         }
     }
 
-    // Initial execution
+    // Initial run
     toggleApiFields();
     $('#iur-upload-method').change(toggleApiFields);
 
-    // Handle settings reset
+    // Reset settings handler
     $('#iur-reset-settings').on('click', function(e) {
         e.preventDefault();
-
+        
         if (confirm(iurSettings.i18n.confirmReset)) {
-            $.post(iurSettings.ajaxurl, {
+            $.post(ajaxurl, {
                 action: 'iur_reset_settings',
                 _wpnonce: iurSettings.nonce
             }, function(response) {
                 if (response.success) {
                     location.reload();
                 } else {
-                    alert(iurSettings.i18n.resetError + ' ' + (response.data || iurSettings.i18n.unknownError));
+                    alert(iurSettings.i18n.error + ': ' + response.data);
                 }
             });
         }
